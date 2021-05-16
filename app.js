b5fb21e95956032fabdd5b2c152de71a404046f5
Paul Thomas <paulthomas2107@gmail.com>
2021-05-16 17:45:19 +0100

    More...

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
+const path = require("path");
 
 // Load config
 dotenv.config({ path: "./config/config.env" });
@@ -9,6 +12,21 @@ dotenv.config({ path: "./config/config.env" });
 connectDB();
 
 const app = express();
+
+// Logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Handlebars
+app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
+
+// Static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.use("/", require("./routes/index"));
 
 const PORT = process.env.PORT || 3000;
 
