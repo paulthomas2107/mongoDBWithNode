8f252a52ee4b5eae67f228e68589cb3bbe0436d6
Paul Thomas <paulthomas2107@gmail.com>
2021-05-17 20:23:36 +0100

    Update

--- routes/stories.js
+++ routes/stories.js
@@ -1,3 +1,4 @@
+const { request } = require("express");
 const express = require("express");
 const router = express.Router();
 const { ensureAuth } = require("../middleware/auth");
@@ -35,6 +36,44 @@ router.get("/", ensureAuth, async (req, res) => {
   } catch (err) {
     console.error(err);
     res.render("error/500");
+  }
+});
+
+//@desc  Show edit page
+//@route GET /stories/edit/:id
+router.get("/edit/:id", ensureAuth, async (req, res) => {
+  const story = await Story.findOne({
+    _id: req.params.id,
+  }).lean();
+
+  if (!story) {
+    return res.render("error/404");
+  }
+
+  if (story.user != req.user.id) {
+    res.redirect("/stories");
+  } else {
+    res.render("stories/edit", { story });
+  }
+});
+
+//@desc  Update Story
+//@route PUT /stories/:id
+router.put("/:id", ensureAuth, async (req, res) => {
+  let story = await Story.findById(req.params.id).lean();
+
+  if (!story) {
+    return res.render("error/404");
+  }
+
+  if (story.user != req.user.id) {
+    res.redirect("/stories");
+  } else {
+    story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.redirect("/dashboard");
   }
 });
 
