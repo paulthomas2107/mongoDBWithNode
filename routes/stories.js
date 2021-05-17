f4dad20684040f85a988aa19ae9f92226f1daa64
Paul Thomas <paulthomas2107@gmail.com>
2021-05-17 21:18:16 +0100

    The End..

--- routes/stories.js
+++ routes/stories.js
@@ -39,6 +39,25 @@ router.get("/", ensureAuth, async (req, res) => {
   }
 });
 
+//@desc  Show Single Story
+//@route GET /stories /:id
+router.get("/:id", ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).populate("user").lean();
+
+    if (!story) {
+      return res.render("error/404");
+    }
+
+    res.render("stories/show", {
+      story,
+    });
+  } catch (err) {
+    console.error(err);
+    res.render("error/404");
+  }
+});
+
 //@desc  Show edit page
 //@route GET /stories/edit/:id
 router.get("/edit/:id", ensureAuth, async (req, res) => {
@@ -93,6 +112,25 @@ router.delete("/:id", ensureAuth, async (req, res) => {
   try {
     await Story.remove({ _id: req.params.id });
     res.redirect("/dashboard");
+  } catch (err) {
+    console.error(err);
+    return res.render("error/500");
+  }
+});
+
+//@desc  User Stories
+//@route GET /stories/user/:userId
+router.get("/user/:userId", ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({
+      user: req.params.userId,
+      status: "public",
+    })
+      .populate("user")
+      .lean();
+    res.render("stories/index", {
+      stories,
+    });
   } catch (err) {
     console.error(err);
     return res.render("error/500");
